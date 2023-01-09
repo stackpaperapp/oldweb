2af74a88aa0633428a92658e315feb08f4798ba2
Mager <andrew.mager@gmail.com>
2023-01-08 18:22:21 -0600

    Dark header

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 
+import useDarkMode from "use-dark-mode";
+import { getDarkModeDescriptor } from "../../utils/accessibility";
+
 type Props = {
   newAccount: boolean;
 };
 
 const Header = ({ newAccount }: Props) => {
+  const darkMode = useDarkMode(false);
+
   return (
     <div className="flex justify-between items-center py-4">
       <div
@@ -13,7 +18,12 @@ const Header = ({ newAccount }: Props) => {
         Budgets
       </div>
       <div>
-        <Image src="/logo-256.png" width={64} height={64} alt="Stack Paper" />
+        <Image
+          src={`/logo-256-${getDarkModeDescriptor(darkMode.value)}.png`}
+          width={64}
+          height={64}
+          alt="Stack Paper"
+        />
       </div>
       <div className="flex-grow-1 basis-0">
         <a href="/api/auth/logout">Logout</a>
